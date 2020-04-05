04da4d6e03c4faa160deeefaf3849e36957ccd1b
Sergey <halygali@mail.ru>
2020-04-05 09:27:11 +0300

    Updates

---
+++ precache-manifest.0040954faada835799029564f4f5ab29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "083c7e03896b45f8757039842fe246df",
+    "url": "/dogs/index.html"
+  },
+  {
+    "revision": "8cebd9d3f92d6ef1a4a3",
+    "url": "/dogs/static/css/main.eff79a23.chunk.css"
+  },
+  {
+    "revision": "6c54c572880cf230d4d8",
+    "url": "/dogs/static/js/2.b36b3da4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dogs/static/js/2.b36b3da4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cebd9d3f92d6ef1a4a3",
+    "url": "/dogs/static/js/main.2b338191.chunk.js"
+  },
+  {
+    "revision": "47857cbbf679eb631aa3",
+    "url": "/dogs/static/js/runtime-main.578f25b7.js"
+  },
+  {
+    "revision": "75fa68d4d95a95385dabee5a76f50797",
+    "url": "/dogs/static/media/dog.75fa68d4.mp3"
+  }
+]);
